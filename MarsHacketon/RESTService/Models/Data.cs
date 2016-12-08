1e216fddc76c30e35ef3969e61dc31f62d019d3f
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-08 12:22:59 +0100

    added service

---
+++ MarsHacketon/RESTService/Models/Data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTService.Models
+{
+    public class Data
+    {
+        public double Temperature
+        {
+            get;
+            set;
+        }
+        public double WindSpeed
+        {
+            get;
+            set;
+        }
+        public WindOrientation WindOrientation
+        {
+            get;
+            set;
+        }
+        public double ParticleSize
+        {
+            get;
+            set;
+        }
+        public string TimeStamp
+        {
+            get;
+            set;
+        }
+        public Data(double Temperature, double WindSpeed, WindOrientation WindOrientation, double ParticleSize, string TimeStamp)
+        {
+            this.Temperature = Temperature;
+            this.WindSpeed = WindSpeed;
+            this.WindOrientation = WindOrientation;
+            this.ParticleSize = ParticleSize;
+            this.TimeStamp = TimeStamp;
+        }
+    }
+}
