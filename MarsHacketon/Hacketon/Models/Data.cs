582767f28cd66465d367d661d2a3eac8e5218397
robin van Lierde <robin2383@hotmail.com>
2016-12-08 10:19:03 +0100

    FDSFDSFDSF

---
+++ MarsHacketon/Hacketon/Models/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hacketon.Models
+{
+    public class Data
+    {
+        public int Temperature;
+
+        public int Windspeed;
+
+        public int WindOrientation;
+
+        public double ParticleSize;
+
+        public DateTimeKind DateTime;
+
+        public Data(int temperature, int windspeed, int windOrientation , double particleSize, DateTimeKind dateTime)
+        {
+            this.Temperature = temperature;
+            this.Windspeed = windspeed;
+
+        }
+    }
+}
--- MarsHacketon/MarsHacketon/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarsHacketon.MvcApplication" Language="C#" %>
