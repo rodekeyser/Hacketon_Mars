dd7309309e7fb1ae848e700c3a9168cc7163ca76
robin van Lierde <robin2383@hotmail.com>
2016-12-08 13:25:52 +0100

    vieuws

--- MarsHacketon/hacketonApp/MainPage.xaml.cs
+++ MarsHacketon/hacketonApp/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +14,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using hacketonApp.Annotations;
+using hacketonApp.Model;
+using LiveCharts;
+using LiveCharts.Configurations;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +26,121 @@ namespace hacketonApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page,INotifyPropertyChanged
     {
+        private double _axisMax;
+        private double _axisMin;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            var mapper = Mappers.Xy<Data>()
+               .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+               .Y(model => model.Temperature);           //use the value property as Y
+
+            //lets save the mapper globally.
+            Charting.For<Data>(mapper);
+
+
+            //the values property will store our values array
+            ChartValues = new ChartValues<Data>();
+
+            //lets set how to display the X Labels
+            DateTimeFormatter = value => new DateTime((long)(value)).ToString("mm:ss");
+
+            AxisStep = TimeSpan.FromSeconds(1).Ticks;
+            SetAxisLimits(DateTime.Now);
+
+            //The next code simulates data changes every 300 ms
+            Timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(300)
+            };
+            Timer.Tick += TimerOnTick;
+            IsDataInjectionRunning = false;
+            R = new Random();
+
+            DataContext = this;
+        }
+
+        public ChartValues<Data> ChartValues { get; set; }
+        public Func<double, string> DateTimeFormatter { get; set; }
+
+        public double AxisStep { get; set; }
+
+        public double AxisMax
+        {
+            get { return _axisMax; }
+            set
+            {
+                _axisMax = value;
+                OnPropertyChanged("AxisMax");
+            }
+        }
+        public double AxisMin
+        {
+            get { return _axisMin; }
+            set
+            {
+                _axisMin = value;
+                OnPropertyChanged("AxisMin");
+            }
+        }
+
+        public DispatcherTimer Timer { get; set; }
+        public bool IsDataInjectionRunning { get; set; }
+        public Random R { get; set; }
+
+        private void RunDataOnClick(object sender, RoutedEventArgs e)
+        {
+            if (IsDataInjectionRunning)
+            {
+                Timer.Stop();
+                IsDataInjectionRunning = false;
+            }
+            else
+            {
+                Timer.Start();
+                IsDataInjectionRunning = true;
+            }
+        }
+
+        private void TimerOnTick(object sender, object eventArgs)
+        {
+            var now = DateTime.Now;
+
+            ChartValues.Add(new Data
+            {
+                Temperature = R.Next(0, 10),
+                DateTime = now,
+             
+            });
+
+            SetAxisLimits(now);
+
+            //lets only use the last 30 values
+            if (ChartValues.Count > 30) ChartValues.RemoveAt(0);
+        }
+
+        private void SetAxisLimits(DateTime now)
+        {
+            AxisMax = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 100ms ahead
+            AxisMin = now.Ticks - TimeSpan.FromSeconds(8).Ticks; //we only care about the last 8 seconds
+        }
+
+
+
+
+
+    
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ MarsHacketon/hacketonApp/Model/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacketonApp.Model
+{
+    public class Data
+    {
+        public double Temperature { get; set; }
+        public double Windspeed { get; set; }
+        public int WindOrientation { get; set; }
+        public double ParticleSize { get; set; }
+
+        public DateTime DateTime;
+
+       
+
+
+
+    }
+}
